593b9a033801880467d60881b4ae1272c5ff6b0c
Martin Suda <sudam@cs.man.ac.uk>
2022-08-25 16:40:18 +0200

    something

--- log_loader.py
+++ log_loader.py
@@ -18,43 +18,14 @@ import sys,random,itertools
 
 from multiprocessing import Pool
 
-def load_one(task):
-  i,logname = task
-  
-  print(i)
-  start_time = time.time()
-  result = IC.load_one(logname) # ,max_size=15000)
-  print("Took",time.time()-start_time)
-  if result:
-    probdata,time_elapsed = result
-    return (logname,time_elapsed),probdata
-  else:
-    None
-
 if __name__ == "__main__":
-  # Experiments with pytorch and torch script
-  # what can be learned from a super-simple TreeNN
-  # which distinguishes:
-  # 1) conj, user_ax, theory_ax_kind in the leaves
-  # 2) what inference leads to this in the tree nodes
+  # Load vampire-lawa log (to be run with "-spt on -p on"), or logs
+  # and turn them into a pickle for learning (updating) a model
   #
-  # Load a set of logs, passed in a file as the final argument, each will become a "training example tree"
-  # Scan the set and create a histogram (to learn which initial and derivational networks will be needed; including the "dropout" defaults)
-  # - save it to "data_hist.pt"
-  # normalize the training data and save that to "training_data.pt" / "validation_data.pt" using a 80:20 split
-  #
-  # To be called as in: ./log_loader.py <folder> *.log-files-listed-line-by-line-in-a-file (an "-s4k on" run of vampire)
-  #
-  # optionally, also a file with problem easyness line-by-line records, e.g. mizar_common/prob_easiness_strats1234.txt
-  #
-  # data_sign.pt and raw_log_data_*.pt are created in <folder>
+  # To be called as in: ./log_loader.py 
+  
 
-  prob_easiness = {}
-  if len(sys.argv) > 3:
-    with open(sys.argv[3],"r") as f:
-      for line in f:
-        spl = line.split()
-        prob_easiness[spl[0]] = int(spl[1])
+  
 
   prob_data_list = [] # [(logname,(init,deriv,pars,selec,good)]
 
