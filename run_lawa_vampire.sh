4c96b70f9ec5a04558a65c4b0c7492814d928412
Martin Suda <martin.suda@cvut.cz>
2022-08-29 18:11:17 +0200

    the wrapper script for running vampire (hardconding some defaults)

---
+++ run_lawa_vampire.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../projects/vampire/z3/build > /dev/null
+export LD_LIBRARY_PATH=`pwd`
+cd - > /dev/null
+
+# echo $1 -t 0 -i 50000 -m 8192 --input_syntax tptp -stat full -sa discount -spt on -si on -rawr on -rtra on --random_seed $$ $3 $2 ">" "$4"/`(./filenamify_path.sh "$2")`.log
+timelimit -t 60 -T 1 $1 -t 0 -i 50000 -m 8192 --input_syntax tptp -stat full -sa discount -si on -rawr on -rtra on --random_seed $$ -spt on $2 $3 > "$4"/`(./filenamify_path.sh "$2")`.log 2>&1
+exit 0
