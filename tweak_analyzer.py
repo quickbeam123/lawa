66c75aee3236aa3c5adaccc4e8c4741faa0ed558
Martin Suda <sudam@cs.man.ac.uk>
2023-01-12 10:37:58 +0100

    take tweaks of arbitrary dim >= 2 (fixing projections to coor 0 and 1, but can be changed), also color the prob points by TPTP ratings

--- tweak_analyzer.py
+++ tweak_analyzer.py
@@ -5,25 +5,42 @@ import os, sys, shutil, pickle, random, atexit, time
 from collections import defaultdict
 
 import torch
+import numpy as np
 
 if __name__ == "__main__":
   # Load a tweak map and do some plotting
   #
   # To be called as in: ./tweak_analyzer.py tweak_map.pt
 
+  x_idx = 0
+  y_idx = 1
+
+  with open("probinfo7.5.0.pkl",'rb') as f:
+    probinfo = pickle.load(f,encoding='utf-8')
+
   tweak_map = torch.load(sys.argv[1])
   Xs = []
   Ys = []
   Ls = []
-  for prob,(x,y) in tweak_map.items():
-    if (x,y) != (0.0,0.0):
-      Xs.append(x)
-      Ys.append(y)
-      Ls.append(prob.split("/")[-1][:-2])
+  Zs = [] # problem TPTP ratings
+
+  for prob,tweak in tweak_map.items():
+    if np.linalg.norm(tweak) > 0.000001:
+      Xs.append(tweak[x_idx])
+      Ys.append(tweak[y_idx])
+      probname = prob.split("/")[-1]
+      Ls.append(probname[:-2])
+
+      info = probinfo[probname]
+      Zs.append(info[0])
+      if info[0] > 0.9:
+        print(probname,info[0])
 
   import matplotlib.pyplot as plt
+  cm = plt.cm.get_cmap('RdYlBu_r')
   fig, ax = plt.subplots()
-  ax.scatter(Xs, Ys)
+  sc = ax.scatter(Xs, Ys, c= Zs, vmin=0, vmax=1, s=35, cmap=cm)
+  plt.colorbar(sc)
 
   for i, l in enumerate(Ls):
     ax.annotate(l, (Xs[i], Ys[i]))
