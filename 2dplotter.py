1bef6d20c824bd6d891341d5c83ab60c5daa0adf
Martin Suda <martin.suda@cvut.cz>
2023-02-17 08:35:32 +0000

    tuning 2dplotter.py

--- 2dplotter.py
+++ 2dplotter.py
@@ -13,33 +13,27 @@ from collections import defaultdict
 MISSIONS = ["train","test"]
 
 if __name__ == "__main__":
-  # Plot the age-weight -> logit graph for a trained model with FEATURE_SUBSET == FEATURES_AW
+  # Plot the age-weight -> logit graph for a trained model with NUM_FEATURES == 2
   #
-  # To be called as in: ./2dplotter.py parts-model.pt (index of the effective queue if > 0 is wished)
+  # To be called as in: ./2dplotter.py loop-model-and-optimizer.tar
 
   model = IC.get_initial_model()
-  model.load_state_dict(torch.load(sys.argv[1]))
-  if len(sys.argv) > 2:
-    idx = int(sys.argv[2])
-  else:
-    idx = 0
-
-  # TODO: index does not work yet
-
-  clause_embedder,clause_keys = model
+  (loop,model_state_dict,optimizer_state_dict) = torch.load(sys.argv[1])
+  model.load_state_dict(model_state_dict)
 
   import numpy as np
   dx, dy = 0.05, 0.05
 
-  x = np.arange(0.0, 100.05, dx)
-  y = np.arange(0.0, 120.05, dy)
+  x = np.arange(-0.1, 20.05, dx)
+  y = np.arange(-0.1, 20.05, dy)
   X, Y = np.meshgrid(x, y)
 
   def get_logit(age, weight):
     with torch.no_grad():
-      features = torch.tensor(np.column_stack((age.ravel(),weight.ravel()))).float()
-      embeddings = clause_embedder(features)
-      logits = torch.matmul(clause_keys.weight,torch.t(embeddings))
+      features = np.column_stack((age.ravel(),weight.ravel()))
+      features = np.pad(features,((0,0),(0,HP.NUM_FEATURES-2)),'constant')
+
+      logits = model(torch.tensor(features).float())
 
       return logits.reshape(age.shape)
 
@@ -48,8 +42,9 @@ if __name__ == "__main__":
   Z = get_logit(X, Y)
 
   from matplotlib import pyplot as plt
-  img = plt.imshow(Z, cmap=plt.cm.hsv, interpolation='nearest',extent=extent, origin='lower')
+  # img = plt.imshow(Z, cmap=plt.cm.hsv, interpolation='nearest',extent=extent, origin='lower')
+  img = plt.contourf(Z,extent=extent, origin='lower')
 
   plt.colorbar()
-  plt.savefig("aw_map.png",dpi=250)
+  plt.savefig(f"aw_map_loop{loop}.png",dpi=250)
 
