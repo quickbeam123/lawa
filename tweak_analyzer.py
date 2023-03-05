438f8acf47aea87a2fc2c2afe0b1ca114598ef45
Martin Suda <sudam@cs.man.ac.uk>
2023-03-05 20:13:41 +0100

    support 3d, optional annotate

--- tweak_analyzer.py
+++ tweak_analyzer.py
@@ -19,6 +19,9 @@ if __name__ == "__main__":
 
   x_idx = 0
   y_idx = 1
+  z_idx = 2
+
+  is3d = False
 
   with open("probinfo8.1.2.pkl",'rb') as f:
     probinfo = pickle.load(f,encoding='utf-8')
@@ -29,32 +32,52 @@ if __name__ == "__main__":
 
   Xs = []
   Ys = []
+  Zs = []
   Ls = []
-  Zs = [] # problem TPTP ratings
+  Rs = [] # problem TPTP ratings
 
   for prob,temp_dict in tweak_map.items():
     if '0.0' not in temp_dict:
       continue
     tweak = temp_dict['0.0']
 
+    probname = prob.split("/")[-1]
+    info = probinfo[probname]
+    #if info[0] < 0.8:
+    #  continue
+
     if np.linalg.norm(tweak) > 0.000001:
       Xs.append(tweak[x_idx])
-      Ys.append(tweak[y_idx])
-      probname = prob.split("/")[-1]
+      if y_idx < len(tweak):
+        Ys.append(tweak[y_idx])
+      else:
+        Ys.append(tweak[x_idx])
+      if is3d:
+        if z_idx < len(tweak):
+          Zs.append(tweak[z_idx])
+        else:
+          Zs.append(0.0)
       Ls.append(probname[:-2])
 
-      info = probinfo[probname]
-      Zs.append(info[0])
+      Rs.append(info[0])
       if info[0] > 0.9:
         print(probname,info[0])
 
   import matplotlib.pyplot as plt
   cm = plt.cm.get_cmap('RdYlBu_r')
-  fig, ax = plt.subplots()
-  sc = ax.scatter(Xs, Ys, c= Zs, vmin=0, vmax=1, s=35, cmap=cm)
+  fig = plt.figure()
+  if is3d:
+    ax = fig.add_subplot(projection='3d')
+    sc = ax.scatter(Xs, Ys, Zs, c= Rs, vmin=0, vmax=1, s=5, cmap=cm)
+  else:
+    ax = fig.add_subplot()
+    sc = ax.scatter(Xs, Ys, c= Rs, vmin=0, vmax=1, s=5, cmap=cm)
+
   plt.colorbar(sc)
 
-  for i, l in enumerate(Ls):
-    ax.annotate(l, (Xs[i], Ys[i]))
+  # show TPTP names
+  if False:
+    for i, l in enumerate(Ls):
+      ax.annotate(l, (Xs[i], Ys[i]))
 
   plt.show()
