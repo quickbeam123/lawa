fc9977ec99066e926f8afb7d344b5236c6f23a7f
Martin Suda <martin.suda@cvut.cz>
2023-08-02 07:40:37 +0200

    updated tweak_analyzer.py

--- tweak_analyzer.py
+++ tweak_analyzer.py
@@ -7,11 +7,6 @@ from collections import defaultdict
 import torch
 import numpy as np
 
-def enum_temp0_tweaks(tweak_map):
-  for _prob,temp_dict in tweak_map.items():
-    if '0.0' in temp_dict:
-      yield temp_dict['0.0']
-
 if __name__ == "__main__":
   # Load a tweak map and do some plotting
   #
@@ -23,12 +18,12 @@ if __name__ == "__main__":
 
   is3d = False
 
-  with open("probinfo8.1.2.pkl",'rb') as f:
+  with open("probinfo8.2.0.pkl",'rb') as f:
     probinfo = pickle.load(f,encoding='utf-8')
 
   tweak_map = torch.load(sys.argv[1])
 
-  print(np.std(list(enum_temp0_tweaks(tweak_map)),axis=0))
+  print(np.std(list(tweak_map.values()),axis=0))
 
   Xs = []
   Ys = []
@@ -36,11 +31,7 @@ if __name__ == "__main__":
   Ls = []
   Rs = [] # problem TPTP ratings
 
-  for prob,temp_dict in tweak_map.items():
-    if '0.0' not in temp_dict:
-      continue
-    tweak = temp_dict['0.0']
-
+  for prob,tweak in tweak_map.items():
     probname = prob.split("/")[-1]
     info = probinfo[probname]
     #if info[0] < 0.8:
@@ -76,7 +67,7 @@ if __name__ == "__main__":
   plt.colorbar(sc)
 
   # show TPTP names
-  if False:
+  if True:
     for i, l in enumerate(Ls):
       ax.annotate(l, (Xs[i], Ys[i]))
 
