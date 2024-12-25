229079a2ca47df87f3ca048d9b77325580dccbfc
Martin Suda <martin.suda@cvut.cz>
2024-12-25 09:54:07 +0000

    added one more helper script

---
+++ bestvalid.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import inf_common as IC
+import hyperparams as HP
+
+import torch
+
+import warnings
+warnings.filterwarnings("ignore", message=r"You are using `torch.load`", category=FutureWarning)
+
+import os, sys, shutil, pickle, random, atexit, time
+
+from collections import defaultdict
+
+if __name__ == "__main__":
+  # Open a bunch of log files and look for the best validation loss (in the first loop)
+  # Print each log files name and the best loss recorded, possibly sorrted by the best loss
+  #
+  # call as in, e.g.: ./bestvalid.py gnnexperlogs/exper100imit*.log
+
+  for logname in sys.argv[1:]:
+    with open(logname, "r") as f:
+      bestloss = 1e9
+      for line in f:
+        '''
+        if line.startswith("loop2"):
+          break
+        '''
+
+        if line.startswith("Eval loss on valid"):
+          loss = float(line.split()[-1])
+          bestloss = min(bestloss, loss)
+
+      print(logname, bestloss)
