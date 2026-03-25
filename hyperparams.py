76496e4a1388981d0bcef6193b2081c37fcef82d
Martin Suda <martin.suda@cvut.cz>
2026-03-25 13:35:50 +0000

    the 3rd EARLY_STOP_ROUND almost never kicks in

--- hyperparams.py
+++ hyperparams.py
@@ -112,7 +112,7 @@ WORTH_REPORTING = 120 # more than this many seconds and a new line goes into det
 # is the best, retrieve that model (unless it's the first and we would not progress), and finish the loop there
 TEST_IMPROVE_WINDOW = 5
 
-NUM_EARLY_STOP_ROUNDS = 3
+NUM_EARLY_STOP_ROUNDS = 2
 
 EARLY_STOP_ON = "dist_to_good"
 
